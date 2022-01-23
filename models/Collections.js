f78443dc3b91fda5b508b69d45ed879c88328367
Nikhilesh <nikhileshbhagavan500@gmail.com>
2022-01-23 13:43:51 +0530

    completed users part

---
+++ models/Collections.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require('express-session');
+
+
+mongoose.connect("mongodb://localhost:27017/freeShareDB");
+const userschema = new mongoose.Schema({
+    username: String,
+    password: String,
+});
+const bookschema = new mongoose.Schema({
+    uuid: String,
+    title: String,
+    department: String,
+    isdepartmentothers: Boolean,
+    subdepartment: String,
+    issubdepartmentothers: Boolean,
+
+    book_img_url: String,
+    book_url: String,
+
+});
+const initialreportschema = new mongoose.Schema({
+    report_book_id: String,
+    reported_email: String,
+    report_otp: String,
+})
+const reportschema = new mongoose.Schema({
+    report_book_id: String,
+    reports: [{ subject: String, message: String }],
+    reported_emails: [String],
+});
+//userschema.plugin(passportlocalmongoose); //automatically do hash+salt our passwords
+
+const usermodel = mongoose.model("user", userschema);
+const bookmodel = mongoose.model("book", bookschema);
+const reportbookmodel = mongoose.model("report", reportschema);
+const initialreportbookmodel = mongoose.model("initialreport", initialreportschema);
+//passport.use(usermodel.createStrategy());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    usermodel.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+module.exports = {
+    User: usermodel,
+    Book: bookmodel,
+    Report: reportbookmodel,
+    InitialReport: initialreportbookmodel,
+}
---
+++ server.js
@@ -0,0 +1,288 @@
+require('dotenv').config()
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const cors = require('cors');
+const md5 = require("md5");
+const models = require("./models/Collections.js");
+const flash = require('connect-flash');
+const express = require("express");
+const path = require("path");
+const bodyparser = require("body-parser");
+const ejs = require('ejs');
+const session = require('express-session');
+const passport = require("passport");
+const nodemailer = require('nodemailer');
+
+
+const app = express();
+app.use(bodyparser.urlencoded({
+    extended: false
+}));
+app.use(cors());
+app.use(flash());
+app.use(bodyparser.json());
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 86400000
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        console.log(file);
+        cb(null, 'uploads/' + path.extname(file.originalname).substring(1));
+    },
+    filename: function(req, file, cb) {
+        cb(null, uuidv4() + path.extname(file.originalname));
+    }
+})
+
+var upload = multer({ storage: storage });
+app.get("/books", function(req, res) {
+    models.Book.find({}, function(err, docs) {
+        if (err) {
+            res.json({ message: "Error" });
+        } else {
+            res.json({
+                message: "Success",
+                books: docs
+            });
+        }
+    });
+
+});
+
+app.get("/uploads/:format/:id", function(req, res) {
+    if (fs.existsSync(path.join(__dirname, "uploads/" + req.params["format"] + "/" + req.params["id"] + "." + req.params["format"]))) {
+        res.sendFile(path.join(__dirname, "uploads/" + req.params["format"] + "/" + req.params["id"] + "." + req.params["format"]));
+    } else {
+        res.render("notfound");
+    }
+
+});
+
+app.post("/api/bookupload", upload.fields([{ name: 'book_img', maxCount: 1 }, { name: 'book_pdf', maxCount: 1 }]), function(req, res) {
+    console.log(req.files, req.body);
+    let img_url = null;
+    let pdf_url = "/" + req.files.book_pdf[0].path.split(".")[0];
+    console.log("hi1");
+
+    if (req.body.isimageuploaded === 'false') {
+        img_url = "/uploads/png/default";
+        console.log("hi2");
+    } else {
+        img_url = "/" + req.files.book_img[0].path.split(".")[0];
+        console.log("hi3");
+
+
+    }
+    const book = new models.Book({
+        uuid: uuidv4(),
+        title: req.body.title,
+        department: req.body.department,
+        isdepartmentothers: req.body.isdepartmentothers == "true",
+        subdepartment: req.body.subdepartment,
+        issubdepartmentothers: req.body.issubdepartmentothers == "true",
+        book_img_url: img_url,
+        book_url: pdf_url,
+        isreported: false,
+    });
+    book.save(function(err) {
+        if (err) {
+            res.json({ message: "error" });
+        }
+        console.log(img_url, pdf_url);
+        console.log(req.body);
+
+
+        res.json({ message: "success" });
+    })
+
+
+});
+
+function generateOTP() {
+
+    // Declare a digits variable 
+    // which stores all digits
+    var digits = '0123456789';
+    let OTP = '';
+    for (let i = 0; i < 6; i++) {
+        OTP += digits[Math.floor(Math.random() * 10)];
+    }
+    return OTP;
+}
+
+app.post("/initialreport", function(req, res) {
+    console.log(req.body);
+    models.Report.findOne({ report_book_id: req.body.book_uuid }, function(err, adventure) {
+        if (err) {
+            res.json({ message: "Error, Click Send OTP again" });
+        } else {
+
+            if (adventure !== null && adventure.reported_emails.includes(req.body.email)) {
+                res.json({ message: "Your last report Haven't been resolved yet. Try Reporting after some days" });
+            } else {
+                models.InitialReport.findOne({ report_book_id: req.body.book_uuid, reported_email: req.body.email },
+                    function(err, initialreport) {
+                        if (err) {
+                            res.json({ message: "Error, Click Send OTP again" });
+                        } else {
+                            let a = generateOTP();
+                            var transporter = nodemailer.createTransport({
+                                service: 'gmail',
+                                auth: {
+                                    user: process.env.EMAIL,
+                                    pass: process.env.PASSWORD
+                                }
+                            });
+                            var mailOptions = {
+                                from: process.env.EMAIL,
+                                to: "" + req.body.email,
+                                subject: 'Email Confirmation OTP for Book Id : ' + req.body.book_uuid,
+                                text: "OTP is" + " " + a
+                            };
+
+                            transporter.sendMail(mailOptions, function(error, info) {
+                                if (error) {
+                                    res.json({ message: "Error, Click Send OTP again" });
+                                } else {
+                                    if (initialreport !== null) {
+                                        initialreport.report_otp = md5(a);
+                                        initialreport.save(function(err) {
+                                            if (err) {
+                                                res.json({ message: "Error, Click Send OTP again" });
+                                            } else {
+                                                res.json({ message: " OTP have been sent to Your Email for Confirmation" });
+                                            }
+                                        });
+
+                                    } else {
+                                        var obj = new models.InitialReport({
+                                            report_book_id: req.body.book_uuid,
+                                            reported_email: req.body.email,
+                                            report_otp: md5(a)
+                                        });
+                                        obj.save(function(err) {
+                                            if (err) {
+                                                res.json({ message: "Error, Click Send OTP again" });
+                                            } else {
+                                                res.json({ message: " OTP have been sent to Your Email for Confirmation" });
+                                            }
+                                        });
+                                    }
+                                }
+                            });
+                        }
+                    });
+
+            }
+
+        }
+    });
+
+});
+app.post("/report", function(req, res) {
+    console.log(req.body);
+    models.InitialReport.findOne({ report_book_id: req.body.report_book_id, reported_email: req.body.report_email },
+        function(err, rep) {
+            if (err) {
+                res.json({ message: "Error , Try Submitting Again After some time..." });
+            } else {
+                if (rep !== null && rep.report_otp === md5(req.body.report_otp)) {
+                    models.Report.findOne({ report_book_id: req.body.report_book_id }, function(err, report) {
+                        if (err) {
+                            res.json({ message: "Error , Try Submitting Again After some time..." });
+                        } else {
+                            if (report !== null) {
+                                report.reports.push({ subject: req.body.report_subject, message: req.body.report_message });
+                                report.reported_emails.push(req.body.report_email);
+                                report.save(function(err) {
+                                    if (err) {
+                                        res.json({ message: "Error , Try Submitting Again After some time..." });
+                                    } else {
+                                        res.json({ message: "success" });
+                                    }
+                                });
+
+                            } else {
+                                const obj = new models.Report({
+                                    report_book_id: req.body.report_book_id,
+                                    reports: [{ subject: req.body.report_subject, message: req.body.report_message }],
+                                    reported_emails: [req.body.report_email],
+
+                                });
+                                obj.save(function(err) {
+                                    if (err) {
+                                        res.json({ message: "Error , Try Submitting Again After some time..." });
+                                    } else {
+                                        res.json({ message: "success" });
+                                    }
+                                });
+                            }
+                        }
+                    });
+                } else {
+                    res.json({ message: "Invalid OTP or You haven't clicked Send OTP... " });
+                }
+            }
+        });
+});
+app.get("/admin", function(req, res) {
+    res.render("adminlogin", { messages: req.flash("error") });
+
+});
+app.post("/admin", function(req, res) {
+    models.User.findOne({ username: req.body.username, password: md5(req.body.password) }, function(err, user) {
+        if (err) {
+            req.flash("error", "Error,Try Again");
+            res.redirect("/admin");
+        }
+        if (user == null) {
+            req.flash("error", "Invalid Username Or Password");
+            res.redirect("/admin");
+        } else {
+            req.login(user, function(error) {
+                if (error) {
+                    req.flash("error", "Error,Try Again");
+                    res.redirect("/admin");
+                }
+                res.redirect("/adminhomepage");
+            })
+        }
+    })
+});
+app.get("/adminhomepage", function(req, res) {
+    if (req.isAuthenticated()) {
+
+        res.send("<a href='/adminlogout'>Logout</a>");
+    } else {
+        res.send("failure");
+    }
+});
+app.get("/adminlogout", function(req, res) {
+
+    req.logout();
+    req.session.destroy(function(err) {
+        res.redirect("/admin");
+    });
+
+});
+app.get("/*", function(req, res) {
+    res.render("index.ejs", { user: "hi" });
+});
+app.listen(8000, () => {
+    console.log("listening to port 8000");
+});
+
+//https://semantic-ui.com/modules/popup.html
